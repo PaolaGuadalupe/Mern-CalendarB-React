f81b96eefaa65332c42aaea7810fc3f4b68cbfe4
Paola de la Rosa <paola-2512@outlook.es>
2024-11-12 13:17:55 -0600

    first commit

---
+++ routes/events.js
@@ -0,0 +1,32 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getEvento, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+const router = Router();
+
+router.use( validarJWT );
+
+// Obtener eventos
+router.get('/', getEvento );
+
+// Crear un nuevo evento
+router.post(
+    '/', 
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+ );
+
+// Actualizar evento
+router.put('/:id', actualizarEvento );
+
+// Borrar evento
+router.delete('/:id', eliminarEvento );
+
+module.exports = router;
